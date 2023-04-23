56e65cb40991f2a8eaa34f632cba677d8709c708
includenull <15929809+includenull@users.noreply.github.com>
2023-04-23 18:38:17 +0100

    Update Vite, change polyfill solution, remove useless UAL packages.

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import viteJsconfigPaths from 'vite-jsconfig-paths'
-import svgrPlugin from 'vite-plugin-svgr'
-
-import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
-
-// polyfills inject workaround for vite 4 incompatibility
-// https://github.com/remorses/esbuild-plugins/issues/24  for solving dev error
-// https://stackoverflow.com/questions/72773373/buffer-is-not-exported-by-vite-browser-externalbuffer for solving build error
-
+import { defineConfig } from 'vite';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import react from '@vitejs/plugin-react';
+import viteJsconfigPaths from 'vite-jsconfig-paths';
+import svgrPlugin from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  optimizeDeps: {
-    esbuildOptions: {
-      // Enable esbuild polyfill plugins
-      plugins: [
-        NodeModulesPolyfillPlugin()
-      ]
-    }
-  },
   plugins: [
-    NodeModulesPolyfillPlugin(),
+    nodePolyfills({
+      // Whether to polyfill `node:` protocol imports.
+      protocolImports: true,
+    }),
     react(),
     viteJsconfigPaths(),
     svgrPlugin(),
   ],
-})
+});
